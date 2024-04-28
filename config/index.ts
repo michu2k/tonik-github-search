eaf766140d723f12bcce40a20e924993ca555904
michu2k <michal.strumpf@gmail.com>
2024-04-28 12:13:12 +0200

    Add page config that will contain all important configs

---
+++ config/index.ts
@@ -0,0 +1,9 @@
+const CONFIG = {
+  GITHUB_API_URL: process.env.GITHUB_API_URL,
+  GITHUB_API_TOKEN: process.env.GITHUB_API_TOKEN,
+  COLLECTION_SIZE: {
+    DEFAULT: 20,
+  },
+};
+
+export {CONFIG};
