e2924d2ef043a5cf3ad1eb2a08c6b431a2e85101
michu2k <michal.strumpf@gmail.com>
2024-04-28 12:30:54 +0200

    Extract url api schema to a separate file

--- hooks/useGitHubApiUrlParams.ts
+++ hooks/useGitHubApiUrlParams.ts
@@ -1,23 +1,10 @@
-import {CONFIG} from "@/config";
+import {GitHubApiUrlParams, gitHubApiUrlParamsSchema} from "@/utils/urlParams";
 import {useSearchParams} from "next/navigation";
-import {z} from "zod";
-
-type GitHubApiUrlParams = z.infer<typeof gitHubApiUrlParamsSchema>;
-
-const gitHubApiUrlParamsSchema = z.object({
-  query: z.string().catch(""),
-  page: z.number().catch(1),
-  order: z.string().catch(""),
-  sort: z.string().catch(""),
-  limit: z.number().catch(CONFIG.COLLECTION_SIZE.DEFAULT),
-});
 
 /** This hook is used to get the URL paramaters that are used in the API. */
 const useGitHubApiUrlParams = (): GitHubApiUrlParams => {
   const searchParams = useSearchParams();
   return gitHubApiUrlParamsSchema.parse(Object.fromEntries(searchParams));
 };
-
-export type {GitHubApiUrlParams};
 
 export {useGitHubApiUrlParams};
---
+++ utils/urlParams.ts
@@ -0,0 +1,17 @@
+import {z} from "zod";
+import {CONFIG} from "@/config";
+
+type GitHubApiUrlParams = z.infer<typeof gitHubApiUrlParamsSchema>;
+
+// Schema definition for the Github API URL parameters
+const gitHubApiUrlParamsSchema = z.object({
+  query: z.string().catch(""),
+  page: z.number().catch(1),
+  order: z.string().catch(""),
+  sort: z.string().catch(""),
+  limit: z.number().catch(CONFIG.COLLECTION_SIZE.DEFAULT),
+});
+
+export type {GitHubApiUrlParams};
+
+export {gitHubApiUrlParamsSchema};
